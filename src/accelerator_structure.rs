ebd94476c5f45de6aa36114cec46281d2f8c5e4c
Lily Haverlag <flannyha@gmail.com>
2024-01-15 17:40:14 +0100

    Add MTLIndirectAccelerationStructureInstanceDescriptor

--- src/accelerator_structure.rs
+++ src/accelerator_structure.rs
@@ -50,6 +50,17 @@ pub struct MTLAccelerationStructureUserIDInstanceDescriptor {
     pub user_id: u32,
 }
 
+#[derive(Clone, Copy, PartialEq, Debug, Default)]
+#[repr(C)]
+pub struct MTLIndirectAccelerationStructureInstanceDescriptor {
+    pub transformation_matrix: [[f32; 3]; 4],
+    pub options: MTLAccelerationStructureInstanceOptions,
+    pub mask: u32,
+    pub intersection_function_table_offset: u32,
+    pub user_id: u32,
+    pub acceleration_structure_id: u64,
+}
+
 pub enum MTLAccelerationStructureDescriptor {}
 
 foreign_obj_type! {
